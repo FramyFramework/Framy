9f41eb0de54ef94dd848c458135ec9f3b6bb6beb
Unknown <mrfibunacci@gmail.com>
2019-02-15 19:22:13 +0100

    style

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,21 +1,21 @@
 <?php
-    define('FRAMY_START', microtime(true));
-    define('ROOT_PATH', realpath(__DIR__."/../"));
+define('FRAMY_START', microtime(true));
+define('ROOT_PATH', realpath(__DIR__."/../"));
 
-    session_start();
+session_start();
 
-    /*---------------------------------------
-    | Register Class Loader
-    |----------------------------------------
-    |
-    | The house made Class auto loader.
-    |
-    */
-    require(ROOT_PATH."/app/framework/Component/ClassLoader/ClassLoader.php");
+/*---------------------------------------
+| Register Class Loader
+|----------------------------------------
+|
+| The house made Class auto loader.
+|
+*/
+require(ROOT_PATH."/app/framework/Component/ClassLoader/ClassLoader.php");
 
-    $autoLoader = new \app\framework\Component\ClassLoader\ClassLoader();
-    $autoLoader->register();
+$autoLoader = new \app\framework\Component\ClassLoader\ClassLoader();
+$autoLoader->register();
 
-    $App = new \app\framework\Component\App\App();
+$App = new \app\framework\Component\App\App();
 
-    include("helper.php");
+include("helper.php");
