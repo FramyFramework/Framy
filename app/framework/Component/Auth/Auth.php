a0333c3877493a0d132a4122b7c5856f26a39132
MrFibunacci <mrfibunacci@gmail.com>
2019-03-14 16:21:42 +0100

    added check method to auth class

--- app/framework/Component/Auth/Auth.php
+++ app/framework/Component/Auth/Auth.php
@@ -11,11 +11,21 @@ namespace app\framework\Component\Auth;
 
 class Auth
 {
+    /**
+     * Get the logged in user model
+     * @return \app\framework\Component\Database\Model\Model|null
+     */
     public static function user()
     {
-        // try to get user from session
-        // if session is not set search for cookie
-        // if cookie is set log user in
-        // return instance of User model
+        return SessionGuard::getInstance()->user();
+    }
+
+    /**
+     * Check if someone is logged in
+     * @return bool
+     */
+    public static function check(): bool
+    {
+        return static::user() !== null;
     }
 }
