872b634e4a8e70ce9520a3a8ddf70ecc861e8fb5
MrFibunacci <mrfibunacci@gmail.com>
2019-04-27 13:50:39 +0200

    User::create() uses query builder

--- app/custom/Models/User.php
+++ app/custom/Models/User.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\custom\Models;
 
-use app\framework\Component\Database\DB;
 use app\framework\Component\Database\Model\Model;
 use app\framework\Component\StdLib\Exception\Exception;
 
@@ -11,31 +10,18 @@ class User extends Model
 
     /**
      * Create new user entry
-     * TODO: redo this as soon as the new QueryBuilder gets implemented
+     *
      * @param array $data
      * @throws Exception
      */
     public static function create(array $data)
     {
-        $userModel   = new User();
-        $query       = "INSERT INTO $userModel->table ";
-        $columnNames = "(";
-        $values      = "(";
+        $user = new User();
 
-        $length = count($data);
-        $i      = 1;
         foreach ($data as $columnName => $value) {
-            $toPrepend = $length <= $i ? "" : ", ";
-
-            $columnNames .= $columnName . $toPrepend;
-            $values      .= "'".$value."'" . $toPrepend;
-
-            $i++;
+            $user->$columnName = $value;
         }
-        $query .= $columnNames .") VALUES ". $values .")";
 
-        if (!DB::insert($query)) {
-            throw new Exception("Could not insert entry to database table: ".$userModel->table, $query);
-        }
+        $user->save();
     }
 }
