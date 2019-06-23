e3e8fe672675495f4fd77d4970648e0fd17dfcef
MrFibunacci <mrfibunacci@gmai.com>
2019-06-23 02:57:46 +0200

    Implemented query builder to relation

--- app/framework/Component/Database/Model/Relations/Relation.php
+++ app/framework/Component/Database/Model/Relations/Relation.php
@@ -9,7 +9,9 @@
 namespace app\framework\Component\Database\Model\Relations;
 
 use app\framework\Component\Database\Model\Model;
-use app\framework\Component\Database\Query\Builder;
+use app\framework\Component\Database\Model\Builder;
+use BadMethodCallException;
+use Error;
 
 abstract class Relation
 {
@@ -35,6 +37,13 @@ abstract class Relation
     protected $related;
 
     /**
+     * Indicates if the relation is adding constraints.
+     *
+     * @var bool
+     */
+    protected static $constraints = true;
+
+    /**
      * Create a new relation instance.
      *
      * @param Builder $query
@@ -45,6 +54,77 @@ abstract class Relation
         $this->query   = $query;
         $this->parent  = $parent;
         $this->related = $query->getModel();
+
+        $this->addConstraints();
     }
 
+    /**
+     * Set the base constraints on the relation query.
+     *
+     * @return void
+     */
+    abstract public function addConstraints();
+
+    /**
+     * Handle dynamic method calls to the relationship.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $result = $this->forwardCallTo($this->query, $method, $parameters);
+
+        if ($result === $this->query) {
+            return $this;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Forward a method call to the given object.
+     *
+     * @param  mixed  $object
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     *
+     * @throws BadMethodCallException
+     */
+    protected function forwardCallTo($object, $method, $parameters)
+    {
+        try {
+            return $object->{$method}(...$parameters);
+        } catch (Error | BadMethodCallException $e) {
+            $pattern = '~^Call to undefined method (?P<class>[^:]+)::(?P<method>[^\(]+)\(\)$~';
+
+            if (! preg_match($pattern, $e->getMessage(), $matches)) {
+                throw $e;
+            }
+
+            if ($matches['class'] != get_class($object) ||
+                $matches['method'] != $method) {
+                throw $e;
+            }
+
+            static::throwBadMethodCallException($method);
+        }
+    }
+
+    /**
+     * Throw a bad method call exception for the given method.
+     *
+     * @param  string  $method
+     * @return void
+     *
+     * @throws BadMethodCallException
+     */
+    protected static function throwBadMethodCallException($method)
+    {
+        throw new BadMethodCallException(sprintf(
+            'Call to undefined method %s::%s()', static::class, $method
+        ));
+    }
 }
