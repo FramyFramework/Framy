e2f7d241bc9a0be820cbd3cb6d321cca569f7735
MrFibunacci <mrfibunacci@gmail.com>
2019-11-13 19:24:47 +0100

    fixed indentation of config files

--- config/app.php
+++ config/app.php
@@ -1,78 +1,78 @@
 <?php
 
-    return [
+return [
 
-        /*-------------------------------------------------------------------------
-        | Application Name
-        |--------------------------------------------------------------------------
-        |
-        | This value is the name of your application. This value is used when the
-        | framework needs to place the application's name in a notification or
-        | any other location as required by the application or its packages.
-        */
+    /*-------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    */
 
-        'name' => 'Framy',
+    'name' => 'Framy',
 
-        /*-------------------------------------------------------------------------
-        | Framy version
-        |--------------------------------------------------------------------------
-        |
-        | Version of Framy needed for situations where you need to have the version
-        | of Framy.
-        */
+    /*-------------------------------------------------------------------------
+    | Framy version
+    |--------------------------------------------------------------------------
+    |
+    | Version of Framy needed for situations where you need to have the version
+    | of Framy.
+    */
 
-        'version' => 'v0.10.1',
+    'version' => 'v0.10.1',
 
-        /*-------------------------------------------------------------------------
-        | Crypting Key
-        |--------------------------------------------------------------------------
-        |
-        | The key is used for crypting data and make your application more secure.
-        */
+    /*-------------------------------------------------------------------------
+    | Crypting Key
+    |--------------------------------------------------------------------------
+    |
+    | The key is used for crypting data and make your application more secure.
+    */
 
-        'CrypKey' => 'SomeRandomString',
+    'CrypKey' => 'SomeRandomString',
 
-        /*-------------------------------------------------------------------------
-        | Application Timezone
-        |--------------------------------------------------------------------------
-        |
-        | Here you may specify the default timezone for your application, which
-        | will be used by the PHP date and date-time functions. We have gone
-        | ahead and set this to a sensible default for you out of the box.
-        */
+    /*-------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    */
 
-        'timezone' => 'UTC',
+    'timezone' => 'UTC',
 
-        /*-------------------------------------------------------------------------
-        | Application Locale Configuration
-        |-------------------------------------------------------------------------
-        |
-        | The application locale determines the default locale that will be used
-        | by the Localization Component. You are free to set this value
-        | to any of the locales which will be supported by the application.
-        */
+    /*-------------------------------------------------------------------------
+    | Application Locale Configuration
+    |-------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the Localization Component. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    */
 
-        'locale' => 'en',
+    'locale' => 'en',
 
-        /*-------------------------------------------------------------------------
-        | Application Fallback Locale
-        |--------------------------------------------------------------------------
-        |
-        | The fallback locale determines the locale to use when the current one
-        | is not available. You may change the value to correspond to any of
-        | the language folders that are provided through your application.
-        */
+    /*-------------------------------------------------------------------------
+    | Application Fallback Locale
+    |--------------------------------------------------------------------------
+    |
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
+    */
 
-        'fallBackLocale' => 'en',
+    'fallBackLocale' => 'en',
 
-        /*-------------------------------------------------------------------------
-        | Debug Mode
-        |--------------------------------------------------------------------------
-        |
-        | Here you can tell Framy rather it should show exceptions or not.
-        | Its highly recommended to set false on production site, because it
-        | could reveal sensible data.
-        */
+    /*-------------------------------------------------------------------------
+    | Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | Here you can tell Framy rather it should show exceptions or not.
+    | Its highly recommended to set false on production site, because it
+    | could reveal sensible data.
+    */
 
-        'debug' => true
-    ];
+    'debug' => true
+];
